e4f7d827ac80a24f01afecfd60be23bb10272038
addisonwebb <addisonwebb@icloud.com>
2018-07-26 21:20:50 -0500

    Initial Commit

---
+++ BrickAndMortar/BrickAndMortar.h
@@ -0,0 +1,19 @@
+//
+//  BrickAndMortar.h
+//  BrickAndMortar
+//
+//  Created by Addison Webb on 7/26/18.
+//  Copyright © 2018 Code Bits LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BrickAndMortar.
+FOUNDATION_EXPORT double BrickAndMortarVersionNumber;
+
+//! Project version string for BrickAndMortar.
+FOUNDATION_EXPORT const unsigned char BrickAndMortarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrickAndMortar/PublicHeader.h>
+
+
